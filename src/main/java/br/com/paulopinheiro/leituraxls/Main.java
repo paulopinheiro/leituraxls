f72eba16bff70109264baddf16a9f79806bcb7ca
2360 <2360@10.12.167.23>
2014-10-24 16:09:10 -0200

    Configurado LookAndFeel para o default do sistema.

--- src/main/java/br/com/paulopinheiro/leituraxls/Main.java
+++ src/main/java/br/com/paulopinheiro/leituraxls/Main.java
@@ -4,10 +4,13 @@ import br.com.paulopinheiro.leituraxls.view.Principal;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import javax.swing.JFrame;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 
 public class Main {
-    public static void main (String[] args) throws FileNotFoundException, IOException, InvalidFormatException {
+    public static void main (String[] args) throws FileNotFoundException, IOException, InvalidFormatException, ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
+        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         JFrame principal = new Principal();
         principal.setVisible(true);
     }
